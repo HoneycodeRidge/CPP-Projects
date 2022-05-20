1bc7a9686d6d000c1c7f2f3446bd1f2fb1cd940d
HoneycodeRidge <105868482+HoneycodeRidge@users.noreply.github.com>
2022-05-20 09:18:26 -0500

    Block Initials - Mini Project
    Mini project to become familiar with the std::cout command and printing to the terminal.

---
+++ block_initials.cpp
@@ -0,0 +1,16 @@
+/* I am 25 years old and learning to code to break into tech! <3 */
+
+#include <iostream>
+
+int main() {
+
+  // My new program!
+  std::cout << "TTTTT   GGG \n";
+  std::cout << "  T    G   G\n";
+  std::cout << "  T    G    \n";
+  std::cout << "  T    GGGGG\n";
+  std::cout << "  T    G   G\n";
+  std::cout << "  T    G   G\n";
+  std::cout << "  T     GGG\n";
+
+}
